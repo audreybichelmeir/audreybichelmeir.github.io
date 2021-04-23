fd126da6c28d69b892b205d8265d3f89de7208cf
audreybichelmeir <bichelmeir@chapman.edu>
2021-04-23 08:18:26 +0000

    got hamburger working

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+const menuBtn = document.querySelector(".menu-btn");
+const menuItems = document.querySelector(".menu-items");
+const menuItem = document.querySelectorAll(".menu-item");
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
+// main toggle
+menuBtn.addEventListener("click", () => {
+  toggle();
 });
 
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-})
+// toggle on item click if open
+menuItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (menuBtn.classList.contains("open")) {
+      toggle();
+    }
+  });
+});
+
+function toggle() {
+  menuBtn.classList.toggle("open");
+  menuItems.classList.toggle("open");
+}
